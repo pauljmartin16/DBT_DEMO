c9d1b71bcd06fb2e96b912b00ca255ed2629ea98
Paul Martin <pauljmartin16@gmail.com>
2025-11-15 15:30:22 +0000

    adding daily_weather.sql

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -6,20 +6,26 @@ WITH CTE AS (
     HOUR(TO_TIMESTAMP(STARTED_AT)) AS HOUR_STARTED_AT,
     DAYNAME(TO_TIMESTAMP(STARTED_AT)),
 
-    CASE
-    WHEN DAYNAME(TO_TIMESTAMP(STARTED_AT)) in ('Sat', 'Sun')
-    THEN 'WEEKEND'
-    ELSE 'BUSINESSDAY'
-    END AS DAY_TYPE,
+    -- CASE
+    -- WHEN DAYNAME(TO_TIMESTAMP(STARTED_AT)) in ('Sat', 'Sun')
+    -- THEN 'WEEKEND'
+    -- ELSE 'BUSINESSDAY'
+    -- END AS DAY_TYPE,
 
-    CASE WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) in (12,1,2)
-        THEN 'WINTER'
-        WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) in (3,4,5)
-        THEN 'SPRING'
-        WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) in (6,7,8)
-        THEN 'SUMMER'
-        ELSE 'AUTUMN'
-    END AS STATION_OF_YEAR
+    {{ day_type('STARTED_AT') }} AS DAY_TYPE,
+
+    -- CASE WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) in (12,1,2)
+    --     THEN 'WINTER'
+    --     WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) in (3,4,5)
+    --     THEN 'SPRING'
+    --     WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) in (6,7,8)
+    --     THEN 'SUMMER'
+    --     ELSE 'AUTUMN'
+    -- END AS STATION_OF_YEAR,
+
+    {{get_season('STARTED_AT')}} AS STATION_OF_YEAR,
+
+    {{function1('STARTED_AT')}}
         
     from
     {{ source('demo', 'bike') }}
